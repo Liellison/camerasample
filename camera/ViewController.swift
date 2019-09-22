2b6898e45e2abd18e7c6f336b8d454f0c0dfce25
Liellison Menezes <liellisonjoseph@gmail.com>
2019-09-22 03:10:45 -0300

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by Liellison Menezes on 22/09/19.
+//  Copyright © 2019 Liellison Menezes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
